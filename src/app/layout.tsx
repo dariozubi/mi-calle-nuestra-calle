ee40e7b6fa4d776d5dd8018f4fd60291b56e8657
Dario Zubillaga <dario@dazu.ma>
2024-02-18 15:17:25 -0600

    feat: add SEO improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,31 @@ export const metadata: Metadata = {
   title: 'mi calle, nuestra calle',
   description:
     'Laboratoria de futuros comunitarios [futuros + arte en contexto + espacios comunes]',
+  generator: 'Next.js',
+  applicationName: 'mi calle, nuestra calle',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'Futuros',
+    'Arte en contexto',
+    'Espacios comunes',
+    'Futuros comunitarios',
+  ],
+  authors: [
+    { name: 'dazuma', url: 'https://dazu.ma' },
+    { name: 'Sophia Arrazola' },
+  ],
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  robots: 'all',
+  openGraph: {
+    title: 'mi calle, nuestra calle',
+    description:
+      'Laboratoria de futuros comunitarios [futuros + arte en contexto + espacios comunes]',
+    images: '/img/nosotras.jpg',
+  },
 }
 
 export default function RootLayout({
