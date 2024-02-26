3cb1577d60b9913c558ea40f0b9e47eeeaae2bea
Dario Zubillaga <dario@dazu.ma>
2024-02-26 17:27:44 -0600

    fix: missing layout static locale

--- src/app/[locale]/(footerless)/layout.tsx
+++ src/app/[locale]/(footerless)/layout.tsx
@@ -1,10 +1,15 @@
 import NavigationBar from '@/components/NavigationBar'
+import { PageProps } from '@/utils/types'
+import { unstable_setRequestLocale } from 'next-intl/server'
 
 export default function RootLayout({
   children,
+  params: { locale },
 }: {
   children: React.ReactNode
+  params: PageProps['params']
 }) {
+  unstable_setRequestLocale(locale)
   return (
     <>
       <NavigationBar />
--- src/app/[locale]/(full)/layout.tsx
+++ src/app/[locale]/(full)/layout.tsx
@@ -1,11 +1,16 @@
 import NavigationBar from '@/components/NavigationBar'
 import Footer from '@/components/Footer'
+import { PageProps } from '@/utils/types'
+import { unstable_setRequestLocale } from 'next-intl/server'
 
 export default function RootLayout({
   children,
+  params: { locale },
 }: {
   children: React.ReactNode
+  params: PageProps['params']
 }) {
+  unstable_setRequestLocale(locale)
   return (
     <>
       <NavigationBar />
