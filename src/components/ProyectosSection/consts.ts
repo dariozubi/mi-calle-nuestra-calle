cef852cba2867dff5bfc9f044e270b61e92d6511
Dario Zubillaga <dario@dazu.ma>
2024-02-29 16:53:03 -0600

    fix: husky

--- src/components/ProyectosSection/consts.ts
+++ src/components/ProyectosSection/consts.ts
@@ -3,7 +3,6 @@ import { proyectas } from '@/utils/consts'
 export const trabajos = [
   ...proyectas.map(t => ({
     image: t.images ? t.images[0] : undefined,
-    titulo: t.titulo,
     link: `/proyecta/${t.slug}`,
     tags: t.tags,
     slug: t.slug,
